829699c5fbc914ef2a31bb300dae62efba0cda78
DouaaAl <elmahraouidouaa123@gmail.com>
2024-10-02 03:13:49 +0000

    10.0

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const photoResult = await uploadPhoto();
+  const userResult = await createUser();
+
+  return {
+    photo: photoResult.status == 200 ? photoResult : null,
+    user: userResult.firstName ? userResult : null,
+  };
+}
