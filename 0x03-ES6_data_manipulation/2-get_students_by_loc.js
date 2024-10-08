fb03c3a841d03b3ab6260fb3bb3adbf606e9366c
DouaaAl <elmahraouidouaa123@gmail.com>
2024-10-08 06:59:55 +0000

    fixing eslint issues task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(students, city) {
-  const newArray = students.filter((student) => student.location == city);
+  const newArray = students.filter((student) => student.location === city);
   return newArray;
 }
