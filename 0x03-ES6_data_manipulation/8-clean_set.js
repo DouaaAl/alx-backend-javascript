c2597757096e7cf33e629ff9b9aec92dbe9c3761
DouaaAl <elmahraouidouaa123@gmail.com>
2024-10-08 08:02:58 +0000

    fixing eslint issues task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,12 @@
+export default function cleanSet(set, startString) {
+  const array = [];  
 
+  for (const item of Array.from(set)) {
+    if (startString === '') {}
+    else if (item.startsWith(startString)) {
+      array.push(item.substring(startString.length).trim());
+    }
+  }
+
+  return array.join('-');
+}
